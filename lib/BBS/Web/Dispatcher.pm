e7a85f4748bb16f648e16b2ae20bbb5417fc8c6d
kazuminn <e145702@ie.u-ryukyu.ac.jp>
2016-10-18 12:10:46 +0900

    first commit

--- lib/BBS/Web/Dispatcher.pm
+++ lib/BBS/Web/Dispatcher.pm
@@ -1,28 +1,32 @@
 package BBS::Web::Dispatcher;
 use strict;
 use warnings;
-use utf8;
-use Amon2::Web::Dispatcher::RouterBoom;
+
+use Amon2::Web::Dispatcher::Lite;
 
 any '/' => sub {
     my ($c) = @_;
-    my $counter = $c->session->get('counter') || 0;
-    $counter++;
-    $c->session->set('counter' => $counter);
-    return $c->render('index.tx', {
-        counter => $counter,
-    });
+
+    my @entries = $c->db->search(
+        entry => {
+        }, {
+            order_by => 'entry_id DESC',
+            limit    => 10,
+        }
+    );
+    return $c->render( "index.tx" => { entries => \@entries, } );
 };
 
-post '/reset_counter' => sub {
-    my $c = shift;
-    $c->session->remove('counter');
-    return $c->redirect('/');
-};
-
-post '/account/logout' => sub {
+post '/post' => sub {
     my ($c) = @_;
-    $c->session->expire();
+
+    if (my $body = $c->req->param('body')) {
+        $c->db->insert(
+            entry => +{
+                body => $body,
+            }
+        );
+    }
     return $c->redirect('/');
 };
 
